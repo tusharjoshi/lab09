c16b5784151f4f70a7d07751377c5a2beaa4570b
Tushar Joshi <tusharvjoshi@gmail.com>
2023-11-22 20:00:29 +0530

    Add AdminController and admin.index view, with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin', [
+    AdminController::class, 'index'
+])->middleware('auth')->name('admin');
